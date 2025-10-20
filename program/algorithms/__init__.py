c0fa50be02e65b8b2133a06a3a6fec2dd29ce6b0
SLey3 <ghub4127@gmail.com>
2025-10-19 22:47:46 -0700

    relative imports fix + full setup complete

--- program/algorithms/__init__.py
+++ program/algorithms/__init__.py
@@ -1,6 +1,6 @@
-from dijkstra import dijkstra
-from gbfs import gbfs
-from astar import astar
+from .dijkstra import dijkstra
+from .gbfs import gbfs
+from .astar import astar
 
 __all__ = [
 	"dijkstra",
